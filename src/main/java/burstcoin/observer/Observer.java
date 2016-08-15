153dd3c327293b730b1945ab549b087784f9a2bd
luxe <luxe@hush.com>
2016-08-15 17:42:25 +0200

    - version 0.2.1-SNAPSHOT - bugfix email disabled

--- src/main/java/burstcoin/observer/Observer.java
+++ src/main/java/burstcoin/observer/Observer.java
@@ -104,15 +104,11 @@ public class Observer
     // overwritten by application.properties
     Map<String, Object> properties = new HashMap<String, Object>();
     properties.put("server.port", ObserverProperties.getObserverPort());
-
-    if(ObserverProperties.isEnableForkNotify())
-    {
-      properties.put("spring.mail.protocol", ObserverProperties.getMailProtocol());
-      properties.put("spring.mail.host", ObserverProperties.getMailHost());
-      properties.put("spring.mail.port", ObserverProperties.getMailPort());
-      properties.put("spring.mail.username", ObserverProperties.getMailUsername());
-      properties.put("spring.mail.password", ObserverProperties.getMailPassword());
-    }
+    properties.put("spring.mail.protocol", ObserverProperties.getMailProtocol());
+    properties.put("spring.mail.host", ObserverProperties.getMailHost());
+    properties.put("spring.mail.port", ObserverProperties.getMailPort());
+    properties.put("spring.mail.username", ObserverProperties.getMailUsername());
+    properties.put("spring.mail.password", ObserverProperties.getMailPassword());
 
     new SpringApplicationBuilder(Observer.class)
       .properties(properties)
